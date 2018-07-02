1561db779d0f9d3c34631ea6a6f2367758b288de
Kachel Wittig <kjwittig@gmail.com>
2018-07-02 15:03:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,13 @@ class Application
   def call
     resp = Rack::Response.new
     time = Time.now
+
+    resp.write time
     if time.hour < 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
     end
   end
+
 end # Application
